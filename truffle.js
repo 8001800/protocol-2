baaaaead7ae1dac700a91bca2c7cfbe715c611af
Pradyuman Vig <pradyumanvig@outlook.com>
2018-02-24 00:11:02 -0600

    add dev and rinkeby to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,31 @@
+require('babel-register')({
+    ignore: /node_modules\/(?!zeppelin-solidity)/
+});
+require('babel-polyfill');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+
+  networks: {
+
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      from: "0x16779b7B0736080843adFAb1ea34C624fC70E0Ff",
+      network_id: 4,
+      gas: 4000000
+    }
+
+  },
+
+  optimizer: {
+    enabled: true,
+    runs: 500
+  },
+
 };
