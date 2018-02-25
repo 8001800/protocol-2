ee0e09d0fa0ad5fd25e3ad6519c381e1a62cb285
Pradyuman Vig <pradyumanvig@outlook.com>
2018-02-25 00:44:17 -0600

    add service requests and callbacks to DebtInstrument

--- contracts/DebtInstrument.sol
+++ contracts/DebtInstrument.sol
@@ -3,14 +3,41 @@ pragma solidity ^0.4.19;
 interface DebtInstrument {
 
   /**
+    * Service Requests
+    */
+
+  function requestComplianceCheck(uint256 id)
+    external returns (uint256 signalId);
+
+  function requestComplianceCheck(uint256 id, uint256 complianceServiceId)
+    external returns (uint256 signalId);
+
+  function requestComplianceCheck(uint256 id, address complianceStandard)
+    external returns (uint256 signalId);
+
+  function requestAppraisal(uint256 id)
+    external returns (uint256 signalId);
+
+  function requestAppraisal(uint256 id, uint256 appraisalServiceId)
+    external returns (uint256 signalId);
+
+  /**
+    * Service Callbacks
+    */
+  
+  function onComplianceCheckFinished(uint256 signalId, bool isCompliant) external;
+
+  function onAppraisalFinished(uint256 signalId, uint256 value, uint256 decimals) external;
+
+  /**
     * Getters for Debt Token information.
     * This allows ComplianceStandards to query information about
     * a debt instrument without needing to know how its data is structured.
     */
 
-  function getBoolField(uint256 id, string field) external returns(bool);
+  function getBoolField(uint256 id, string field) external returns (bool);
 
-  function getInt256Field(uint256 id, string field) external returns (uint256);
+  function getInt256Field(uint256 id, string field) external returns (int256);
 
   function getUint256Field(uint256 id, string field) external returns (uint256);
 
