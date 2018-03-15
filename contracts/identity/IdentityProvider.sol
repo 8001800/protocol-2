aa5f6d255f3e706ca10c86ae4144cb30372fb4e5
Ian Macalinao <me@ian.pw>
2018-03-14 21:57:45 -0500

    provider registry upgrade

--- contracts/identity/IdentityProvider.sol
+++ contracts/identity/IdentityProvider.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.19;
 
-contract IdentityProvider {
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../provider/ProviderRegistry.sol";
+
+contract IdentityProvider is Ownable {
     function getBoolField(address user, string field) view external returns (bool);
 
     function getInt256Field(address user, string field) view external returns (int256);
@@ -18,4 +21,11 @@ contract IdentityProvider {
     function getBytesField(address user, string field) view external returns (bytes);
 
     function getStringField(address user, string field) view external returns (string);
+
+    function performUpgrade(
+        ProviderRegistry providerRegistry, string nextMetadata, address nextProvider
+    ) onlyOwner external returns (bool)
+    {
+        return providerRegistry.upgradeProvider(nextMetadata, nextProvider);
+    }
 }
