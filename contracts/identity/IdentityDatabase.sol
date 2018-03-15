1fc8cfbfbeae7b38e9562e12fc49d8fac865219c
Ian Macalinao <me@ian.pw>
2018-03-14 21:45:37 -0500

    identity provider specification

--- contracts/identity/IdentityDatabase.sol
+++ contracts/identity/IdentityDatabase.sol
@@ -2,19 +2,4 @@ pragma solidity ^0.4.19;
 
 import "../provider/ProviderRegistry.sol";
 
-contract IdentityDatabase is ProviderRegistry {
-
-    function addField(
-        address target,
-        string version,
-        string key,
-        string value
-    ) external returns (bool);
-
-    function getField(
-        address target,
-        string version,
-        string key
-    ) external returns (string);
-
-}
+contract IdentityDatabase is ProviderRegistry {}
---
+++ contracts/identity/IdentityProvider.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.19;
+
+contract IdentityProvider {
+    function getBoolField(address user, string field) view external returns (bool);
+
+    function getInt256Field(address user, string field) view external returns (int256);
+
+    function getUint256Field(address user, string field) view external returns (uint256);
+
+    function getFixedField(address user, string field) view external returns (fixed);
+
+    function getUfixedField(address user, string field) view external returns (ufixed);
+
+    function getAddressField(address user, string field) view external returns (address);
+
+    function getBytes32Field(address user, string field) view external returns (bytes32);
+
+    function getBytesField(address user, string field) view external returns (bytes);
+
+    function getStringField(address user, string field) view external returns (string);
+}
