2de8bdaab2cc350bccbf98986a55115a58b35b78
Ian Macalinao <me@ian.pw>
2018-03-17 17:14:06 -0500

    remove appraisal

--- contracts/provider/Upgradable.sol
+++ contracts/provider/Upgradable.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.4.19;
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../provider/ProviderRegistry.sol";
 
+/**
+ * @title Upgradable
+ * @dev A contract which can be upgraded in a ProviderRegistry.
+ */
 contract Upgradable is Ownable {
 
     /**
-     * Upgrades this provider to a new address.
+     * @dev Upgrades this provider to a new address.
+     *
+     * @param providerRegistry The registry on which to enact the upgrade.
+     * @param nextMetadata The metadata of the next provider.
+     * @param nextProvider The address of the next provider.
+     * @return True if the upgrade was successful.
      */
     function performUpgrade(
         ProviderRegistry providerRegistry, string nextMetadata, address nextProvider
