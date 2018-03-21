cddb137a21aafbb2996b1b513da56c8b4748c254
Ian Macalinao <me@ian.pw>
2018-03-21 11:01:33 -0500

    identity coordinator docs

--- contracts/identity/IdentityCoordinator.sol
+++ contracts/identity/IdentityCoordinator.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.4.19;
 import "../NeedsAbacus.sol";
 import "../provider/ProviderRegistry.sol";
 
+/**
+ * @title IdentityCoordinator
+ * @dev Coordinates identity service providers.
+ * Identity providers subscribe to `IdentityVerificationRequested` events and provide their
+ * services, writing the results on-chain.
+ */
 contract IdentityCoordinator is NeedsAbacus {
     ProviderRegistry public providerRegistry;
 
@@ -10,29 +16,56 @@ contract IdentityCoordinator is NeedsAbacus {
         providerRegistry = _providerRegistry;
     }
 
+    /**
+     * @dev Emitted when an identity verification is requested.
+     * This event is to be subscribed to and read by the provider off-chain.
+     * Once the off-chain provider sees this event and ensures the correct cost
+     * has been paid, they are to perform the identity verification then update the state
+     * of their IdentityProvider.
+     *
+     * @param providerId The id of the provider.
+     * @param providerVersion The version of the provider when the request was made.
+     * @param user The address of the user that requests identity services.
+     * @param args Any arguments that the identity provider may need.
+     * @param cost The amount the user paid to the compliance provider.
+     * @param requestId An arbitrary id to link the request to the off-chain database.
+     */
     event IdentityVerificationRequested(
-        uint256 providerId,
+        uint256 indexed providerId,
+        uint256 providerVersion,
         address user,
         string args,
         uint256 cost,
-        uint256 requestToken
+        uint256 requestId
     );
 
     /**
      * @dev Requests verification of identity from a provider.
+     *
+     * @param providerId The id of the provider.
+     * @param args Any arguments that the identity provider may need.
+     * @param cost The amount the user paid to the compliance provider.
+     * @param requestId An arbitrary id to link the request to the off-chain database.
      */
     function requestVerification(
         uint256 providerId,
         string args,
         uint256 cost,
-        uint256 requestToken
+        uint256 requestId
     ) external returns (bool)
     {
         address owner = providerRegistry.providerOwner(providerId);
         if (!kernel.transferTokensFrom(msg.sender, owner, cost)) {
             return false;
         }
-        IdentityVerificationRequested(providerId, msg.sender, args, cost, requestToken);
+        IdentityVerificationRequested(
+            providerId,
+            providerRegistry.latestProviderVersion(providerId),
+            msg.sender,
+            args,
+            cost,
+            requestToken
+        );
     }
 
 }
