48a54336db7a0e07ee071ce68a6b30d4830fb5ce
Ian Macalinao <me@ian.pw>
2018-03-22 18:08:09 -0500

    Get compliance standard deploying

--- contracts/instruments/AbacusInstrument.sol
+++ contracts/instruments/AbacusInstrument.sol
@@ -7,23 +7,40 @@ contract AbacusInstrument {
     * This allows ComplianceStandards to query information about
     * an instrument without needing to know how its data is structured.
     */
+  function getBoolField(uint256 id, string field) view external returns (bool) {
+    assert(false);
+  }
 
-  function getBoolField(uint256 id, string field) view external returns (bool);
+  function getInt256Field(uint256 id, string field) view external returns (int256) {
+    assert(false);
+  }
 
-  function getInt256Field(uint256 id, string field) view external returns (int256);
+  function getUint256Field(uint256 id, string field) view external returns (uint256) {
+    assert(false);
+  }
 
-  function getUint256Field(uint256 id, string field) view external returns (uint256);
+  // function getFixedField(uint256 id, string field) view external returns (fixed) {
+  //   assert(false);
+  // }
 
-  function getFixedField(uint256 id, string field) view external returns (fixed);
+  // function getUfixedField(uint256 id, string field) view external returns (ufixed) {
+  //   assert(false);
+  // }
 
-  function getUfixedField(uint256 id, string field) view external returns (ufixed);
+  function getAddressField(uint256 id, string field) view external returns (address) {
+    assert(false);
+  }
 
-  function getAddressField(uint256 id, string field) view external returns (address);
+  function getBytes32Field(uint256 id, string field) view external returns (bytes32) {
+    assert(false);
+  }
 
-  function getBytes32Field(uint256 id, string field) view external returns (bytes32);
+  function getBytesField(uint256 id, string field) view external returns (bytes) {
+    assert(false);
+  }
 
-  function getBytesField(uint256 id, string field) view external returns (bytes);
-
-  function getStringField(uint256 id, string field) view external returns (string);
+  function getStringField(uint256 id, string field) view external returns (string) {
+    assert(false);
+  }
 
 }
