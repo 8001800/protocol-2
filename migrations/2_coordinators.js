81ae4f25aebb66bf344f383ff51dc6b94a416e23
Ian Macalinao <me@ian.pw>
2018-03-26 00:22:05 -0500

    fix provider registry deployment

--- migrations/2_coordinators.js
+++ migrations/2_coordinators.js
@@ -3,6 +3,7 @@ const ComplianceCoordinator = artifacts.require("ComplianceCoordinator");
 const IdentityCoordinator = artifacts.require("IdentityCoordinator");
 
 module.exports = async deployer => {
+  await deployer.deploy(ProviderRegistry);
   await deployer.deploy(ComplianceCoordinator, ProviderRegistry.address);
   await deployer.deploy(IdentityCoordinator, ProviderRegistry.address);
 };
