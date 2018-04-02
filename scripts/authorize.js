20ada3481f38e6190b05188023dd1847aff550a8
Ian Macalinao <me@ian.pw>
2018-04-02 16:31:59 -0500

    take in account as function param

--- scripts/authorize.js
+++ scripts/authorize.js
@@ -4,9 +4,8 @@ const BooleanIdentityProvider = artifacts.require("BooleanIdentityProvider");
 const IDENTITY_COORDINATOR_ADDR = "0x212cb0ce7da0334166092b782f456a8ab33c9e6f";
 const BOOLEAN_IDENTITY_PROVIDER_ADDR =
   "0x72b780c66f71e203cd50f77908854bcaffec2e86";
-const ACCOUNT = "0x259ddecd8542c7af8e5cb74629c405f11c866e50";
 
-const main = async () => {
+const main = async (account: string) => {
   const cost = 0;
   const requestId = Math.floor(Math.random() * 1000000);
 
@@ -22,14 +21,14 @@ const main = async () => {
     "",
     cost,
     requestId,
-    { from: ACCOUNT }
+    { from: account }
   );
 
-  await identityProvider.addPassing(ACCOUNT, requestId);
+  await identityProvider.addPassing(account, requestId);
 };
 
 module.exports = cb =>
-  main()
+  main(ACCOUNT)
     .then(res => cb(null, res))
     .catch(err => cb(err));
 
