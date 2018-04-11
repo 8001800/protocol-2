06fcfe96cf03fc7c1b0f86f31646c82edb0580b9
Ian Macalinao <me@ian.pw>
2018-04-10 22:19:19 -0500

    lol

--- contracts/compliance/ComplianceCoordinator.sol
+++ contracts/compliance/ComplianceCoordinator.sol
@@ -16,11 +16,20 @@ contract ComplianceCoordinator is AbacusCoordinator {
     }
 
     /**
-     * @dev Stores the status of an asynchronous compliance check.
+     * @dev Mapping of actionId => requestId.
      */
-    struct CheckStatus {
+    mapping (uint256 => uint256) actionRequests;
+
+    uint256 nextRequestId = 1;
+
+    struct Request {
         /**
-         * @dev Block when this check status has expired.
+         * @dev Id of the escrow associated with this request.
+         */
+        uint256 escrowId;
+
+        /**
+         * @dev Block when this check status has expired. 0 if we haven't writen.
          */
         uint256 blockToExpire;
 
@@ -28,12 +37,17 @@ contract ComplianceCoordinator is AbacusCoordinator {
          * @dev Result of the check. 0 indicates success, non-zero is left to the caller.
          */
         uint8 checkResult;
+
+        /**
+         * @dev Id of the action associated with the request.
+         */
+        uint256 actionId;
     }
 
     /**
-     * @dev Mapping of action id => check status.
+     * @dev Mapping of all requestIds to the request.
      */
-    mapping (uint256 => CheckStatus) public statuses;
+    mapping (uint256 => Request) requests;
 
     /**
      * @dev Emitted when a compliance check is performed.
@@ -67,6 +81,7 @@ contract ComplianceCoordinator is AbacusCoordinator {
      * has been paid, they are to perform a compliance check then call `writeCheckResult`
      * with the appropriate parameters.
      *
+     * @param requestId The id of the compliance check request.
      * @param providerId The id of the provider.
      * @param providerVersion The version of the provider.
      * @param instrumentAddr The address of the instrument contract.
@@ -77,6 +92,7 @@ contract ComplianceCoordinator is AbacusCoordinator {
      * @param cost The amount the user paid to the compliance provider.
      */
     event ComplianceCheckRequested(
+        uint256 requestId,
         uint256 indexed providerId,
         uint256 providerVersion,
         address instrumentAddr,
@@ -90,30 +106,20 @@ contract ComplianceCoordinator is AbacusCoordinator {
     /**
      * @dev Emitted when the result of an asynchronous compliance check is written.
      *
-     * @param providerId The id of the provider.
-     * @param providerVersion The version of the provider.
-     * @param instrumentAddr The address of the instrument contract.
-     * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
-     * @param from The from address of the token transfer.
-     * @param to The to address of the token transfer.
-     * @param data Any additional data related to the action.
+     * @param providerId The id of the compliance check request.
      * @param blockToExpire The block in which the compliance check result expires.
      * @param checkResult The result of the compliance check.
      */
     event ComplianceCheckResultWritten(
-        uint256 providerId,
-        uint256 providerVersion,
-        address instrumentAddr,
-        uint256 instrumentIdOrAmt,
-        address from,
-        address to,
-        bytes32 data,
+        uint256 requestId,
         uint256 blockToExpire,
         uint8 checkResult
     );
 
     /**
      * @dev Requests a compliance check from a Compliance Provider.
+     * NOTE: this overwrites any existing escrow for this check. Make sure you don't have a pending
+     * escrow when calling this!
      *
      * @param providerId The id of the provider.
      * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
@@ -121,6 +127,7 @@ contract ComplianceCoordinator is AbacusCoordinator {
      * @param to The to address of the token transfer.
      * @param data Any additional data related to the action.
      * @param cost The amount the user paid to the compliance provider.
+     * @param expiryBlocks The blocks until the check request expires.
      */
     function requestCheck(
         uint256 providerId,
@@ -129,67 +136,14 @@ contract ComplianceCoordinator is AbacusCoordinator {
         address from,
         address to,
         bytes32 data,
-        uint256 cost
-    ) external returns (bool)
+        uint256 cost,
+        uint256 expiryBlocks
+    ) external returns (uint256)
     {
         address owner = providerRegistry.providerOwner(providerId);
-        if (!kernel.transferTokensFrom(msg.sender, owner, cost)) {
-            return false;
-        }
-        emit ComplianceCheckRequested(
-            providerId,
-            providerRegistry.latestProviderVersion(providerId),
-            instrumentAddr,
-            instrumentIdOrAmt,
-            from,
-            to,
-            data,
-            cost
-        );
-    }
-
-    /**
-     * @dev Writes the result of an asynchronous compliance check to the blockchain.
-     *
-     * @param providerId The id of the provider.
-     * @param providerVersion The version of the provider.
-     * @param instrumentAddr The address of the instrument contract.
-     * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
-     * @param from The from address of the token transfer.
-     * @param to The to address of the token transfer.
-     * @param data Any additional data related to the action.
-     * @param blockToExpire The block in which the compliance check result expires.
-     * @param checkResult The result of the compliance check.
-     */
-    function writeCheckResult(
-        uint256 providerId,
-        uint256 providerVersion,
-        address instrumentAddr,
-        uint256 instrumentIdOrAmt,
-        address from,
-        address to,
-        bytes32 data,
-        uint256 blockToExpire,
-        uint8 checkResult
-    ) external returns (bool)
-    {
-        uint256 id;
-        address owner;
-        uint256 version;
-        (id,,, owner,version,) = providerRegistry.latestProvider(providerId);
-
-        // Check service exists
-        if (id == 0) {
-            return false;
-        }
-        // Check provider version is correct
-        if (version != providerVersion) {
-            return false;
-        }
-        // Check service owner is correct
-        if (owner != msg.sender) {
-            return false;
-        }
+        require(kernel.transferTokensFrom(msg.sender, owner, cost));
+        uint256 escrowId = kernel.beginEscrow(msg.sender, owner, cost, expiryBlocks);
+        require(escrowId != 0);
 
         uint256 actionId = computeActionId(
             providerId,
@@ -201,10 +155,66 @@ contract ComplianceCoordinator is AbacusCoordinator {
             data
         );
 
-        statuses[actionId] = CheckStatus({
-            blockToExpire: blockToExpire,
-            checkResult: checkResult
+        uint256 requestId = nextRequestId++;
+        Request storage request = Request({
+            escrowId: escrowId,
+            blockToExpire: 0,
+            checkResult: 0,
+            actionId: actionId
         });
+        requests[requestId] = request;
+
+        emit ComplianceCheckRequested({
+            requestId: requestId,
+            providerId: providerId,
+            providerVersion: providerRegistry.latestProviderVersion(providerId),
+            instrumentAddr: instrumentAddr,
+            instrumentIdOrAmt: instrumentIdOrAmt,
+            from: from,
+            to: to,
+            data: data,
+            cost: cost
+        });
+    }
+
+    /**
+     * @dev Writes the result of an asynchronous compliance check to the blockchain.
+     *
+     * @param requestId The id of the request.
+     * @param blockToExpire The block in which the compliance check result expires.
+     * @param checkResult The result of the compliance check.
+     */
+    function writeCheckResult(
+        uint256 requestId,
+        uint256 blockToExpire,
+        uint8 checkResult
+    ) external returns (bool)
+    {
+        uint256 id;
+        address owner;
+        uint256 version;
+        (id,,, owner,version,) = providerRegistry.latestProvider(providerId);
+
+        // Check service exists
+        require(id != 0);
+        // Check provider version is correct
+        require(version == providerVersion);
+        // Check service owner is correct
+        require(owner == msg.sender);
+
+        uint256 actionId = computeActionId(
+            providerId,
+            providerVersion,
+            instrumentAddr,
+            instrumentIdOrAmt,
+            from,
+            to,
+            data
+        );
+
+        requests[]
+        actionRequests[actionId] = requestId;
+
         emit ComplianceCheckResultWritten(
             providerId,
             providerVersion,
