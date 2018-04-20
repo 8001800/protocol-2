c85bdee4b18f4d99e97b0ba1d2b5c7912d9ab266
Ian Macalinao <me@ian.pw>
2018-04-20 18:53:56 -0500

    Fix all migration

--- migrations/2_protocol.js
+++ migrations/2_protocol.js
@@ -56,51 +56,51 @@ module.exports = async deployer => {
     const sip = await SandboxIdentityProvider.deployed();
     await sip.registerProvider("SandboxIdentity", "", false);
 
-    // // bool compliance standard
-    // await deployer.deploy(
-    //   BooleanSandboxComplianceStandard,
-    //   IdentityToken.address,
-    //   ProviderRegistry.address,
-    //   0,
-    //   await sip.providerId()
-    // );
-    // const bscs = await BooleanSandboxComplianceStandard.deployed();
-    // await bscs.registerProvider("BooleanSandbox", "", false);
-    // const bscsId = await bscs.providerId();
-    // console.log("BSCS provider id:", bscsId.toString());
+    // bool compliance standard
+    await deployer.deploy(
+      BooleanSandboxComplianceStandard,
+      IdentityToken.address,
+      ProviderRegistry.address,
+      0,
+      await sip.providerId()
+    );
+    const bscs = await BooleanSandboxComplianceStandard.deployed();
+    await bscs.registerProvider("BooleanSandbox", "", false);
+    const bscsId = await bscs.providerId();
+    console.log("BSCS provider id:", bscsId.toString());
 
-    // // bool compliance standard
-    // await deployer.deploy(
-    //   UintSandboxComplianceStandard,
-    //   IdentityToken.address,
-    //   ProviderRegistry.address,
-    //   0,
-    //   await sip.providerId()
-    // );
-    // const uscs = await UintSandboxComplianceStandard.deployed();
-    // await uscs.registerProvider("UintSandbox", "", false);
-    // const uscsId = await uscs.providerId();
-    // console.log("USCS provider id:", uscsId.toString());
+    // bool compliance standard
+    await deployer.deploy(
+      UintSandboxComplianceStandard,
+      IdentityToken.address,
+      ProviderRegistry.address,
+      0,
+      await sip.providerId()
+    );
+    const uscs = await UintSandboxComplianceStandard.deployed();
+    await uscs.registerProvider("UintSandbox", "", false);
+    const uscsId = await uscs.providerId();
+    console.log("USCS provider id:", uscsId.toString());
 
-    // // compliant token
-    // await deployer.deploy(
-    //   SampleCompliantToken,
-    //   ComplianceCoordinator.address,
-    //   bscsId
-    // );
+    // compliant token
+    await deployer.deploy(
+      SampleCompliantToken,
+      ComplianceCoordinator.address,
+      bscsId
+    );
 
-    // // compliant token
-    // await deployer.deploy(
-    //   SampleCompliantToken2,
-    //   ComplianceCoordinator.address,
-    //   bscsId
-    // );
+    // compliant token
+    await deployer.deploy(
+      SampleCompliantToken2,
+      ComplianceCoordinator.address,
+      bscsId
+    );
 
-    // // compliant token
-    // await deployer.deploy(
-    //   SampleCompliantToken3,
-    //   ComplianceCoordinator.address,
-    //   uscsId
-    // );
+    // compliant token
+    await deployer.deploy(
+      SampleCompliantToken3,
+      ComplianceCoordinator.address,
+      uscsId
+    );
   });
 };
