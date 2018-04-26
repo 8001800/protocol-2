56cb7ac5deaa30d4b2991d31292d9c336f9f0da1
Ian Macalinao <me@ian.pw>
2018-04-26 00:15:05 -0500

    add outside US compliance standard

---
+++ contracts/test/cs/OutsideUSCS.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.4.21;
+
+import "../identity/IdentityToken.sol";
+import "../AnnotationDatabase.sol";
+import "../compliance/ComplianceStandard.sol";
+
+contract OutsideUSCS is ComplianceStandard {
+    IdentityToken identityToken;
+
+    uint256 operations = 0;
+    uint256 identityProviderId;
+    uint256 constant public FIELD_NON_US = 1776;
+
+    function OutsideUSCS(
+        IdentityToken _identityToken,
+        ProviderRegistry _providerRegistry,
+        uint256 _providerId,
+        uint256 _identityProviderId
+    ) Provider(_providerRegistry, _providerId) public
+    {
+        identityToken = _identityToken;
+        identityProviderId = _identityProviderId;
+    }
+
+    function performCheck(
+        address token,
+        uint256,
+        address from,
+        address to,
+        bytes32 
+    ) view external returns (uint8, uint256)
+    {
+        bytes32 fromNonUsVal;
+        (,fromNonUsVal) = identityToken.annotationDatabase().bytes32Data(
+            identityToken, identityToken.tokenOf(from), identityProviderId, FIELD_NON_US
+        );
+        bool fromNonUs = fromNonUsVal != bytes32(0);
+
+        // initial issuance
+        if (token == from) {
+            fromNonUs = true;
+        }
+
+        bytes32 toNonUsVal;
+        (,toNonUsVal) = identityToken.annotationDatabase().bytes32Data(
+            identityToken, identityToken.tokenOf(to), identityProviderId, FIELD_NON_US
+        );
+        bool toNonUs = toNonUsVal != bytes32(0);
+
+        if (fromNonUs && toNonUs) {
+            return (0, 0);
+        }
+
+        uint8 err = 0x10;
+        if (fromNonUs) {
+            err |= 0x8;
+        }
+        if (toNonUs) {
+            err |= 0x2;
+        }
+        return (err, 0);
+    }
+
+    function performHardCheck(
+        address,
+        uint256,
+        address,
+        address,
+        bytes32
+    ) external
+    {
+        operations++;
+    }
+
+}
