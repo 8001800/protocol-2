87d9244d9fa4f277eed7e44e958cfb9bb7e202ea
Ian Macalinao <me@ian.pw>
2018-05-05 09:42:40 -0500

    Add bundle compliance checks

--- contracts/Bundle.sol
+++ contracts/Bundle.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.21;
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "./compliance/ComplianceCoordinator.sol";
 
 /**
  * @dev A collection of ERC20 and ERC721 assets represented as an ERC721 token.
@@ -15,23 +16,30 @@ contract Bundle is ERC721Token {
     mapping (uint256 => mapping (address => uint256)) erc20Assets;
     mapping (uint256 => mapping (address => mapping (uint256 => bool))) erc721Assets;
     mapping (uint256 => bool) locked;
+    mapping (uint256 => uint256) complianceProviderIds;
 
-    event BundleCreate(uint256 indexed bundleId, address owner);
-    event BundleDepositERC20(uint256 indexed bundleId, address token, uint256 amount);
-    event BundleWithdrawERC20(uint256 indexed bundleId, address token, uint256 amount);
-    event BundleDepositERC721(uint256 indexed bundleId, address token, uint256 id);
-    event BundleWithdrawERC721(uint256 indexed bundleId, address token, uint256 id);
-    event BundleLock(uint256 indexed bundleId);
-    event BundleUnlock(uint256 indexed bundleId);
+    event Create(uint256 indexed bundleId, address owner);
+    event DepositERC20(uint256 indexed bundleId, address token, uint256 amount);
+    event WithdrawERC20(uint256 indexed bundleId, address token, uint256 amount);
+    event DepositERC721(uint256 indexed bundleId, address token, uint256 id);
+    event WithdrawERC721(uint256 indexed bundleId, address token, uint256 id);
+    event Lock(uint256 indexed bundleId);
+    event Unlock(uint256 indexed bundleId);
 
-    function Bundle() public ERC721Token("Bundle", "BND") {
+    ComplianceCoordinator complianceCoordinator;
+
+    function Bundle(
+        ComplianceCoordinator _complianceCoordinator
+    ) public ERC721Token("Bundle", "BND")
+    {
+        complianceCoordinator = _complianceCoordinator;
     }
 
     function create(string _uri) external returns (uint256) {
         uint256 id = nextTokenId++;
         _mint(msg.sender, id);
         _setTokenURI(id, _uri);
-        emit BundleCreate(id, msg.sender);
+        emit Create(id, msg.sender);
     }
 
     function depositERC20Asset(
@@ -44,7 +52,7 @@ contract Bundle is ERC721Token {
             return false;
         }
         erc20Assets[_bundleId][_token] = erc20Assets[_bundleId][_token].add(_amount);
-        emit BundleDepositERC20(_bundleId, _token, _amount);
+        emit DepositERC20(_bundleId, _token, _amount);
         return true;
     }
 
@@ -54,11 +62,14 @@ contract Bundle is ERC721Token {
         if (msg.sender != ownerOf(_bundleId)) {
             return false;
         }
+        if (locked[_bundleId]) {
+            return false;
+        }
         if (!_token.transfer(msg.sender, _amount)) {
             return false;
         }
         erc20Assets[_bundleId][_token] = erc20Assets[_bundleId][_token].sub(_amount);
-        emit BundleWithdrawERC20(_bundleId, _token, _amount);
+        emit WithdrawERC20(_bundleId, _token, _amount);
         return true;
     }
 
@@ -70,7 +81,7 @@ contract Bundle is ERC721Token {
         }
         _token.transferFrom(msg.sender, this, _id);
         erc721Assets[_bundleId][_token][_id] = true;
-        emit BundleDepositERC721(_bundleId, _token, _id);
+        emit DepositERC721(_bundleId, _token, _id);
         return true;
     }
 
@@ -80,12 +91,15 @@ contract Bundle is ERC721Token {
         if (msg.sender != ownerOf(_bundleId)) {
             return false;
         }
+        if (locked[_bundleId]) {
+            return false;
+        }
         if (!erc721Assets[_bundleId][_token][_id]) {
             return false;
         }
         _token.safeTransferFrom(this, msg.sender, _id);
         erc721Assets[_bundleId][_token][_id] = false;
-        emit BundleWithdrawERC721(_bundleId, _token, _id);
+        emit WithdrawERC721(_bundleId, _token, _id);
         return true;
     }
 
@@ -96,7 +110,7 @@ contract Bundle is ERC721Token {
             return false;
         }
         locked[_bundleId] = true;
-        emit BundleLock(_bundleId);
+        emit Lock(_bundleId);
         return true;
     }
 
@@ -107,17 +121,45 @@ contract Bundle is ERC721Token {
             return false;
         }
         locked[_bundleId] = false;
-        emit BundleUnlock(_bundleId);
+        emit Unlock(_bundleId);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _tokenId) public {
         require(locked[_tokenId]);
+        if (complianceProviderIds[_tokenId] != 0) {
+            uint8 result;
+            (result,) = complianceCoordinator.hardCheck(
+                complianceProviderIds[_tokenId],
+                this,
+                _tokenId,
+                _from,
+                _to,
+                bytes32(0)
+            );
+            if (result != 0) {
+                return;
+            }
+        }
         super.transferFrom(_from, _to, _tokenId);
     }
 
     function safeTransferFrom(address _from, address _to, uint256 _tokenId) public {
         require(locked[_tokenId]);
+        if (complianceProviderIds[_tokenId] != 0) {
+            uint8 result;
+            (result,) = complianceCoordinator.hardCheck(
+                complianceProviderIds[_tokenId],
+                this,
+                _tokenId,
+                _from,
+                _to,
+                bytes32(0)
+            );
+            if (result != 0) {
+                return;
+            }
+        }
         super.safeTransferFrom(_from, _to, _tokenId);
     }
 
@@ -128,6 +170,20 @@ contract Bundle is ERC721Token {
         bytes _data
     ) public {
         require(locked[_tokenId]);
+        if (complianceProviderIds[_tokenId] != 0) {
+            uint8 result;
+            (result,) = complianceCoordinator.hardCheck(
+                complianceProviderIds[_tokenId],
+                this,
+                _tokenId,
+                _from,
+                _to,
+                bytes32(0)
+            );
+            if (result != 0) {
+                return;
+            }
+        }
         super.safeTransferFrom(_from, _to, _tokenId, _data);
     }
 
