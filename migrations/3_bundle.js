7426fd150c8874d8e82cd5da22baa56ffa93ac8f
Ian Macalinao <me@ian.pw>
2018-05-24 15:30:58 -0500

    fix bundle deployment

--- migrations/3_bundle.js
+++ migrations/3_bundle.js
@@ -1,5 +1,6 @@
 const Bundle = artifacts.require("Bundle");
+const ComplianceCoordinator = artifacts.require("ComplianceCoordinator");
 
 module.exports = async deployer => {
-  await deployer.deploy(Bundle);
+  await deployer.deploy(Bundle, ComplianceCoordinator.address);
 };
