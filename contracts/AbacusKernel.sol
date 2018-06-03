111ac9439b6c53cf2c190aad072015d875cc666c
Pradyuman Vig <pradyumanvig@outlook.com>
2018-06-03 14:33:02 -0700

    create escrow functions in kernel

--- contracts/AbacusKernel.sol
+++ contracts/AbacusKernel.sol
@@ -31,8 +31,104 @@ contract AbacusKernel {
     }
 
     /**
-    * @dev Transfers ABA from an approved person to another.
-    */
+     * @dev Information needed to facilitate Escrow
+     * The Escrow lifecycle has three steps:
+     * - open: an escrow account is created between two parties
+     */
+
+    enum EscrowState {
+        OPEN,
+        LOCKED,
+        CLOSED,
+        CANCELED,
+        EXPIRED
+    }
+
+    struct Escrow {
+        address from;
+        address to;
+        uint256 amount;
+        uint256 blockExpiresAt;
+        EscrowState state;
+    }
+
+    /**
+     * @dev Mapping of escrow id -> escrow.
+     */
+    mapping (uint256 => Escrow) escrows;
+
+    /**
+     * @dev The next id for an escrow.
+     */
+    uint256 nextEscrowId;
+
+    /**
+     * @dev Opens an escrow account between two parties.
+     */
+    function openEscrow(
+        address from,
+        address to,
+        uint256 amount
+    ) internal returns (uint256 escrowId)
+    {
+        require(from != address(0) || to != address(0));
+
+        // Transfer tokens to kernel
+        require(token.transferFrom(from, this, amount));
+
+        escrowId = nextEscrowId++;
+        escrows[escrowId] = Escrow({
+            from: from,
+            to: to,
+            amount: amount,
+            blockExpiresAt: 0,
+            state: EscrowState.OPEN
+        });
+    }
+
+    /**
+     * @dev Locks ABA in escrow account
+     */
+    function lockEscrow(
+        uint256 escrowId,
+        uint256 blocksToExpiry
+    ) internal {
+        Escrow storage escrow = escrows[escrowId];
+        require(escrow.state == EscrowState.OPEN);
+
+        escrow.state = EscrowState.LOCKED;
+        escrow.blockExpiresAt = blocksToExpiry.add(block.number);
+    }
+
+    /**
+     * @dev Closes escrow account and transfers ABA to appropriate party
+     */
+    function closeEscrow(
+        uint256 escrowId
+    ) internal {
+        Escrow storage escrow = escrows[escrowId];
+        require(escrow.state == EscrowState.LOCKED);
+        require(escrow.blockExpiresAt >= block.number);
+
+        require(token.transfer(escrow.to, escrow.amount));
+        escrow.state = EscrowState.CLOSED;
+    }
+
+    /**
+     * @dev Revokes an escrow agreement, either due to cancellation or expiry
+     */
+    function revokeEscrow(
+        uint256 escrowId
+    ) internal {
+        Escrow storage escrow = escrows[escrowId];
+        require(escrow.state == EscrowState.OPEN || escrow.blockExpiresAt >= block.number);
+        require(token.transfer(escrow.from, escrow.amount));
+        escrow.state = escrow.state == EscrowState.OPEN ? EscrowState.CANCELED : EscrowState.EXPIRED;
+    }
+
+    /**
+     * @dev Transfers ABA from an approved person to another.
+     */
     function transferTokensFrom(
         address from,
         address to,
