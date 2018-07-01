d505456c7c2fa46072a5a9f0b820b713203182ea
Ian Macalinao <me@ian.pw>
2018-07-01 13:29:43 -0700

    run prettier on test dir

--- test/bundle.js
+++ test/bundle.js
@@ -24,7 +24,7 @@ contract("Bundle", accounts => {
     providerRegistry = await ProviderRegistry.deployed();
     complianceCoordinator = await ComplianceCoordinator.deployed();
     bundle = await Bundle.deployed();
- 
+
     // Create NFTs
     NFT1 = await SampleNFT.new(1);
     NFT2 = await SampleNFT.new(2);
@@ -39,59 +39,37 @@ contract("Bundle", accounts => {
     await NFT2.mint(accounts[1]);
     await NFT2.mint(accounts[2]);
 
-    // Approve NFTs to Bundle Contract    
-    await NFT1.approve(
-      bundle.address,
-      0,
-      {from: accounts[1]}
-    );
+    // Approve NFTs to Bundle Contract
+    await NFT1.approve(bundle.address, 0, { from: accounts[1] });
 
-    
-    await NFT1.approve(
-      bundle.address, 
-      1,
-      {from: accounts[2]}
-    );
+    await NFT1.approve(bundle.address, 1, { from: accounts[2] });
 
-    await NFT2.approve(
-      bundle.address,
-      0,
-      {from: accounts[1]}
-    );
+    await NFT2.approve(bundle.address, 0, { from: accounts[1] });
 
-    await NFT2.approve(
-      bundle.address,
-      1,
-      {from: accounts[2]}
-    );
+    await NFT2.approve(bundle.address, 1, { from: accounts[2] });
 
     // Distribute Tokens
     await token1.transfer(accounts[3], 1000);
     await token1.transfer(accounts[4], 1000);
     await token2.transfer(accounts[3], 1000);
     await token2.transfer(accounts[4], 1000);
-    
 
     // Approve Bundle Contract
-    await token1.approve(bundle.address, 
-      new BigNumber(2).pow(256).minus(1),
-      {from: accounts[3]}
-    );
+    await token1.approve(bundle.address, new BigNumber(2).pow(256).minus(1), {
+      from: accounts[3]
+    });
 
-    await token1.approve(bundle.address, 
-      new BigNumber(2).pow(256).minus(1),
-      {from: accounts[4]}
-    );
+    await token1.approve(bundle.address, new BigNumber(2).pow(256).minus(1), {
+      from: accounts[4]
+    });
 
-    await token2.approve(bundle.address, 
-      new BigNumber(2).pow(256).minus(1),
-      {from: accounts[3]}
-    );
+    await token2.approve(bundle.address, new BigNumber(2).pow(256).minus(1), {
+      from: accounts[3]
+    });
 
-    await token2.approve(bundle.address, 
-      new BigNumber(2).pow(256).minus(1),
-      {from: accounts[4]}
-    );
+    await token2.approve(bundle.address, new BigNumber(2).pow(256).minus(1), {
+      from: accounts[4]
+    });
   });
 
   it("should create bundle", async () => {
@@ -101,7 +79,10 @@ contract("Bundle", accounts => {
     };
 
     // Create Bundle
-    const { logs } = await bundle.create( params.uri, params.complianceProviderId);
+    const { logs } = await bundle.create(
+      params.uri,
+      params.complianceProviderId
+    );
     bundle1 = logs[1].args.bundleId.toNumber();
     var owner = await bundle.ownerOf(bundle1);
     assert.equal(logs.length, 2);
@@ -148,14 +129,9 @@ contract("Bundle", accounts => {
     var t1BundleBalance = await token1.balanceOf(bundle.address);
     assert.equal(t1BundleBalance.toNumber(), 100);
 
-    await bundle.depositERC20Asset(
-      bundle1,
-      token1.address,
-      400,
-      {
-        from: accounts[4]
-      }
-    );
+    await bundle.depositERC20Asset(bundle1, token1.address, 400, {
+      from: accounts[4]
+    });
 
     var token1InBundle = await bundle.erc20Assets(bundle1, token1.address);
     assert.equal(token1InBundle, 500);
@@ -164,9 +140,9 @@ contract("Bundle", accounts => {
     assert.equal(t1BundleBalance.toNumber(), 500);
     var t2BundleBalance = await token2.balanceOf(bundle.address);
     assert.equal(t2BundleBalance.toNumber(), 200);
-  })
+  });
 
-  it('deposit NFT to Bundle', async() => {
+  it("deposit NFT to Bundle", async () => {
     var acc1NFT1Id = await NFT1.tokenOfOwnerByIndex(accounts[1], 0);
 
     const { logs: depNFT1Logs } = await bundle.depositERC721Asset(
@@ -179,10 +155,10 @@ contract("Bundle", accounts => {
     );
 
     var acc1NFT1Id0InBundle = await bundle.erc721Assets(
-      bundle1, 
-      NFT1.address, 
+      bundle1,
+      NFT1.address,
       acc1NFT1Id
-    )
+    );
 
     assert.equal(acc1NFT1Id0InBundle, true);
     assert.equal(depNFT1Logs.length, 3);
@@ -190,22 +166,21 @@ contract("Bundle", accounts => {
     assert.equal(depNFT1Logs[2].args.bundleId.toNumber(), bundle1);
     assert.equal(depNFT1Logs[2].args.token, NFT1.address);
     assert.equal(depNFT1Logs[2].args.id.toNumber(), acc1NFT1Id.toNumber());
-  })
+  });
 
-  it(`lock bundle`, async() => {
+  it(`lock bundle`, async () => {
     const { logs: lockLogs } = await bundle.lock(bundle1);
     var lockStatus = await bundle.locked(bundle1);
     assert.equal(lockStatus, true);
     assert.equal(lockLogs[0].event, "Lock");
     assert.equal(lockLogs[0].args.bundleId.toNumber(), bundle1);
-  })
+  });
 
-  it(`unlock bundle`, async() => {
+  it(`unlock bundle`, async () => {
     const { logs: unlockLogs } = await bundle.unlock(bundle1);
     var lockStatus = await bundle.locked(bundle1);
     assert.equal(lockStatus, false);
     assert.equal(unlockLogs[0].event, "Unlock");
     assert.equal(unlockLogs[0].args.bundleId.toNumber(), bundle1);
-  })
-  
+  });
 });
