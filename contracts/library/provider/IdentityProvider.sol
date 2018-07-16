2a70d1a15a27e2426f440ed11786ee12cef5c061
dontsellshort <michaelhly@gmail.com>
2018-07-15 19:32:04 -0700

    moved functions from IdentityProvider to AsyncProvider

--- contracts/library/provider/IdentityProvider.sol
+++ contracts/library/provider/IdentityProvider.sol
@@ -6,46 +6,18 @@ import "./AsyncProvider.sol";
 
 contract IdentityProvider is AsyncProvider {
     IdentityToken identityToken;
-    AnnotationDatabase annotationDatabase;
 
     constructor(
         IdentityToken _identityToken,
         AbacusKernel _kernel,
         uint256 _providerId
-    ) AsyncProvider(_kernel, _providerId) public
+    ) AsyncProvider(
+        _kernel, 
+        _identityToken.annotationDatabase(),
+        _providerId
+        ) public
     {
         identityToken = _identityToken;
-        annotationDatabase = _identityToken.annotationDatabase();
-    }
-
-    function writeBytes32Field(
-        address tokenAddr,
-        uint256 tokenId,
-        uint256 fieldId,
-        bytes32 value
-    ) public onlyRole("admin") {
-        annotationDatabase.writeBytes32Field(
-            tokenAddr,
-            tokenId,
-            providerId,
-            fieldId,
-            value
-        );
-    }
-
-    function writeBytesField(
-        address tokenAddr,
-        uint256 tokenId,
-        uint256 fieldId,
-        bytes value
-    ) public onlyRole("admin") {
-        annotationDatabase.writeBytesField(
-            tokenAddr,
-            tokenId,
-            providerId,
-            fieldId,
-            value
-        );
     }
 
     function writeIdentityBytes32Field(
