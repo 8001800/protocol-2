cde10d0d92d4665a567103a2d276bf79f5264ebb
Pradyuman Vig <pradyumanvig@outlook.com>
2018-07-23 14:09:58 -0700

    fix comment format

--- contracts/library/compliance/ComplianceStandard.sol
+++ contracts/library/compliance/ComplianceStandard.sol
@@ -7,17 +7,18 @@ import "../provider/Provider.sol";
  * @dev An on-chain Compliance Provider.
  */
 contract ComplianceStandard is Provider {
+
     /**
-    * @dev Checks to see if an action is compliant.
-    *
-    * @param instrumentAddr The address of the instrument contract.
-    * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
-    * @param from The from address of the token transfer.
-    * @param to The to address of the token transfer.
-    * @param data Any additional data related to the action.
-    *
-    * @return result and next serviceId.
-    */
+     * @dev Checks to see if an action is compliant.
+     *
+     * @param instrumentAddr The address of the instrument contract.
+     * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
+     * @param from The from address of the token transfer.
+     * @param to The to address of the token transfer.
+     * @param data Any additional data related to the action.
+     *
+     * @return result and next serviceId.
+     */
     function performCheck(
         address instrumentAddr,
         uint256 instrumentIdOrAmt,
@@ -27,14 +28,14 @@ contract ComplianceStandard is Provider {
     ) view external returns (uint256, uint256);
 
     /**
-    * @dev Called when a hard check is performed.
-    *
-    * @param instrumentAddr The address of the instrument contract.
-    * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
-    * @param from The from address of the token transfer.
-    * @param to The to address of the token transfer.
-    * @param data Any additional data related to the action.
-    */
+     * @dev Called when a hard check is performed.
+     *
+     * @param instrumentAddr The address of the instrument contract.
+     * @param instrumentIdOrAmt The instrument id (NFT) or amount (ERC20).
+     * @param from The from address of the token transfer.
+     * @param to The to address of the token transfer.
+     * @param data Any additional data related to the action.
+     */
     function performHardCheck(
         address instrumentAddr,
         uint256 instrumentIdOrAmt,
